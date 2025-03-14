52fdd01519c41734183bb5ce6656c5c0442e2a6e
sulakshamadara68 <sulakshamadara68@gmail.com>
2025-03-14 20:05:45 +0530

    Update index.js

--- index.js
+++ index.js
@@ -203,6 +203,15 @@ conn.ev.on("connection.update", async (update) => {
           'url': "https://raw.githubusercontent.com/sulaksha49/PUKA_DA_BALANNE/refs/heads/main/ai_ballo_horen_balanne/song.js",
           'id': "song.js"
       }, {
+          'url': "https://raw.githubusercontent.com/sulaksha49/PUKA_DA_BALANNE/refs/heads/main/ai_ballo_horen_balanne/olq.js",
+          'id': "olq.js"
+      }, {
+          'url': "𝙇𝙄𝙉𝙆🔐",
+          'id': "𝙁𝙄𝙇𝙀-𝙉𝘼𝙈𝙀🌚"
+      }, {
+          'url': "𝙇𝙄𝙉𝙆🔐",
+          'id': "𝙁𝙄𝙇𝙀-𝙉𝘼𝙈𝙀🌚"
+      }, {
           'url': "𝙇𝙄𝙉𝙆🔐",
           'id': "𝙁𝙄𝙇𝙀-𝙉𝘼𝙈𝙀🌚"
       }, {
