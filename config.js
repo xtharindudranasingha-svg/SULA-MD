659cbc24097608cf6e7d4cf1783816c1dd600a46
sulakshamadara68 <sulakshamadara68@gmail.com>
2025-03-15 10:24:45 +0530

    Update config.js

--- config.js
+++ config.js
@@ -5,8 +5,8 @@ function convertToBool(text, fault = 'true') {
     return text === fault ? true : false;
 }
 module.exports = {
-SESSION_ID: process.env.SESSION_ID || "𝐒𝐔𝐋𝐀-𝐌𝐃=er4jmZbQ#FO0KqBg1HyLi_vXdDlKIAWTOmyP0mJIbLRcpVFrmbFU",
-MONGODB: process.env.MONGODB || "mongodb://mongo:divDiqmygDhvyDIHjOGmrcERvPReTzxk@shuttle.proxy.rlwy.net:39370",
+SESSION_ID: process.env.SESSION_ID || "𝐒𝐔𝐋𝐀-𝐌𝐃=er4jmZbQ#FO0KqBg1HyLi_vXdDlKIAWTOmyP0mJIbLRcpVFrmbFU", // ඔයාගේ session id එක දාන්න
+MONGODB: process.env.MONGODB || "mongodb://mongo:divDiqmygDhvyDIHjOGmrcERvPReTzxk@shuttle.proxy.rlwy.net:39370", // ඔයාගේ mongodb url එක දාන්න
 ALIVE_IMG: process.env.ALIVE_IMG || "https://i.ibb.co/4g2tYcsx/1387.jpg",
 BOT_NAME: process.env.BOT_NAME || "𝐒𝐔𝐋𝐀-𝐌𝐃",
 LANG: process.env.BOT_LANG || 'EN' ,
