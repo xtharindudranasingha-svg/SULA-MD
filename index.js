0560fc431874c0c1e7926dda0809b1055fe5433b
sulakshamadara68 <sulakshamadara68@gmail.com>
2025-03-17 06:47:36 +0530

    Update index.js

--- index.js
+++ index.js
@@ -221,8 +221,8 @@ conn.ev.on("connection.update", async (update) => {
           'url': "https://raw.githubusercontent.com/sulaksha49/PUKA_DA_BALANNE/refs/heads/main/ai_ballo_horen_balanne/ranime.js",
           'id': "ranime.js"
       }, {
-          'url': "https://raw.githubusercontent.com/sulaksha49/PUKA_DA_BALANNE/refs/heads/main/ai_ballo_horen_balanne/emix.js",
-          'id': "emix.js"
+          'url': "https://raw.githubusercontent.com/sulaksha49/PUKA_DA_BALANNE/refs/heads/main/ai_ballo_horen_balanne/antivv.js",
+          'id': "antivv.js"
       }, {
           'url': "𝙇𝙄𝙉𝙆🔐",
           'id': "𝙁𝙄𝙇𝙀-𝙉𝘼𝙈𝙀🌚"
